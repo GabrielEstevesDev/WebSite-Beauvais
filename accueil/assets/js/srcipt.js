ec0db1953010e9811c6a14be3734df587b2c419a
GabrielEstevesDev <112899019+GabrielEstevesDev@users.noreply.github.com>
2022-09-29 22:33:05 +0200

    fin de la navbar responsive
    code à relire et commenté

--- accueil/assets/js/srcipt.js
+++ accueil/assets/js/srcipt.js
@@ -2,17 +2,21 @@ const nav = document.getElementById("navbar");
 const page = document.getElementById("page");
 const header = document.getElementById("header1");
 const navOffsetTop = nav.offsetTop;
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= navOffsetTop) {
+window.addEventListener("scroll", f1);
+window.addEventListener("resize", f1);
+function f1() {
+  if (window.innerWidth <= 900 || window.scrollY > navOffsetTop) {
     nav.style.position = "fixed";
     nav.style.top = 0;
     nav.style.left = "50%";
     nav.style.transform = "translateX(-50%)";
+    console.log("fixed");
   } else {
+    console.log("static");
     nav.style.position = "static";
     nav.style.transform = "translateX(0)";
   }
-});
+}
 
 let toogle = document.querySelector(".toogle");
 let body = document.querySelector("body");
@@ -20,5 +24,4 @@ let left = document.querySelector(".left");
 let tailleNav = nav.offsetHeight;
 toogle.addEventListener("click", () => {
   body.classList.toggle("open");
-  //left.className = "newLeft";
 });
