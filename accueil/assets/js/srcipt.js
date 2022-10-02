07ed28d3cbfc5918076e0562c3d68844d24d5b61
GabrielEstevesDev <112899019+GabrielEstevesDev@users.noreply.github.com>
2022-10-02 23:39:00 +0200

    fin de la page d'accueil
    commencement de la mise en commun du projet

--- accueil/assets/js/srcipt.js
+++ accueil/assets/js/srcipt.js
@@ -1,27 +1,33 @@
-const nav = document.getElementById("navbar");
-const page = document.getElementById("page");
-const header = document.getElementById("header1");
-const navOffsetTop = nav.offsetTop;
-window.addEventListener("scroll", f1);
-window.addEventListener("resize", f1);
-function f1() {
-  if (window.innerWidth <= 900 || window.scrollY > navOffsetTop) {
+var nav;
+var page;
+var body;
+var toogle;
+var header;
+
+window.addEventListener("load", init);
+function init() {
+  toogle = document.querySelector(".toogle");
+  body = document.querySelector("body");
+  nav = document.getElementById("navbar");
+  page = document.getElementById("page");
+  header = document.querySelector("header");
+  window.addEventListener("scroll", navPosition);
+  window.addEventListener("resize", navPosition);
+  toogle.addEventListener("click", navResp);
+}
+
+function navPosition() {
+  if (window.innerWidth <= 900 || window.scrollY > header.offsetHeight) {
     nav.style.position = "fixed";
     nav.style.top = 0;
     nav.style.left = "50%";
     nav.style.transform = "translateX(-50%)";
-    console.log("fixed");
   } else {
-    console.log("static");
     nav.style.position = "static";
     nav.style.transform = "translateX(0)";
   }
 }
 
-let toogle = document.querySelector(".toogle");
-let body = document.querySelector("body");
-let left = document.querySelector(".left");
-let tailleNav = nav.offsetHeight;
-toogle.addEventListener("click", () => {
+function navResp() {
   body.classList.toggle("open");
-});
+}
